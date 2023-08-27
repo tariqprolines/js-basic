22a7fe9ccaf48c531e8876d65693aaea99d5587f
Mohd Tariq Khan <51340540+tariqprolines@users.noreply.github.com>
2023-08-27 16:33:42 +0000

    String done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,16 @@
+const name="Tariq";
+const repoCount = 40;
+
+console.log(`Hello ${name}, your repo count is ${repoCount}`);
+
+const getName = new String('tariq-google-com')
+
+console.log(getName.length);
+
+console.log(getName.toUpperCase())
+
+console.log(getName.charAt(2))
+
+console.log(getName.indexOf('r'))
+
+
