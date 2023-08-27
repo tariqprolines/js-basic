2d707b659d09631ae31011772d795245f655b905
Mohd Tariq Khan <51340540+tariqprolines@users.noreply.github.com>
2023-08-27 08:54:50 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let percentage = "33abc"
+
+// console.log(typeof percentage);
+
+let valueInNumber = Number(percentage)
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false = 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1 => true, 0 => false
+
+//"" => false
+
+//"tariq" => true 
+
+let score = 33
+let stringScore = String(score)
+
+console.log(stringScore);
+console.log(typeof stringScore);
+
