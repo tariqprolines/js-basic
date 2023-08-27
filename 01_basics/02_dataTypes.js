7187d734ca3609c2506905380bb4c51d19f9982f
Mohd Tariq Khan <51340540+tariqprolines@users.noreply.github.com>
2023-08-27 08:22:52 +0000

    DataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as a newer version
+
+//alert(3+3) // We are using Node Js, not browser
+
+console.log(3 
+    + 
+    3) // Code readability should be high
+
+console.log("Tariq")    
+
+// Primitive dataType
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// Non Primitive
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
