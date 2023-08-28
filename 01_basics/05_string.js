eb498aa5cf0fdb899c64a2d58d92dcb83e5021d9
Mohd Tariq Khan <51340540+tariqprolines@users.noreply.github.com>
2023-08-28 03:47:38 +0000

    Add string function

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -13,4 +13,39 @@ console.log(getName.charAt(2))
 
 console.log(getName.indexOf('r'))
 
+let name = "2+2"
+
+// console.log(eval(name))
+
+// console.log(eval(new String('2+2')))
+
+// console.log(eval("2+2") === eval("4"))
+
+// console.log(eval(new String('2+2')) === eval('4'))
+
+let fName = "Mohd Tariq"
+let lName = "Khan"
+
+let message = `  Hello khan, how are you, i belive you are good     `
+
+// console.log(fName.concat(' ', lName))
+
+// console.log(fName.charAt(2))
+
+// console.log(fName.indexOf('T'))
+
+// console.log(fName.toLowerCase())
+
+// console.log(fName.toLocaleLowerCase())
+
+// console.log(message.lastIndexOf('you'))
+
+// console.log(message.substr(1,5))
+
+// console.log(message.slice(-9,-5))
+
+// console.log(message.split(" "))
+
+console.log(message.trim())
+
 
