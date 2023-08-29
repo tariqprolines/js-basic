15e5172a983de82f80b39dd3ec0bea6ba82c5650
Mohd Tariq Khan <51340540+tariqprolines@users.noreply.github.com>
2023-08-29 13:17:01 +0000

    Js Array basic done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// Array
+
+const myarr = [1,2,3,4,5]
+
+const myarr2 = new Array(1,2,3,4,5)
+
+// console.log(myarr[2]);
+
+// Array Methods
+
+// myarr.push(6)
+// myarr.push(7)
+
+// myarr.pop()
+
+// myarr.unshift(9)
+
+// myarr.shift();
+
+// console.log(myarr);
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(4));
+
+// const newArr = myarr.join();
+
+// console.log(myarr);
+// console.log(newArr);
+
+
+// slice, splice
+
+console.log("A ", myarr)
+
+const myn1 = myarr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myarr)
+
+console.log(myarr.splice(1,3));
+console.log("C ", myarr);
